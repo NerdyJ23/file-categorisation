0add72b5770015fc5a366e1316675bc111caf5a0
NerdyJ23 <jessica.moolenschot@outlook.com>
2023-04-11 21:41:59 +1000

    Add template data; use autcomplete for filter selection

--- site/src/components/GalleryFilterCard.vue
+++ site/src/components/GalleryFilterCard.vue
@@ -5,25 +5,41 @@
 				<v-text-field
 					append-icon="mdi-magnify"
 					label="Artist"
+					v-model="filter.artist"
 				/>
 			</v-col>
 		</v-row>
 		<v-row>
 			<v-col>
-				<v-combobox
+				<v-autocomplete
 					label="Character(s)"
+					deletable-chips
 					multiple
-					chips
-				></v-combobox>
+					small-chips
+					hide-selected
+					hide-no-data
+					:items="filter.characters.list"
+					v-model="filter.characters.selected"
+				></v-autocomplete>
 			</v-col>
 		</v-row>
 		<v-row>
 			<v-col>
 				<v-autocomplete
 					label="Categories"
-					chips
-					:items="filter.categories"
+					deletable-chips
+					multiple
+					small-chips
+					hide-selected
+					hide-no-data
+					:items="filter.categories.list"
+					v-bind="filter.categories.selected"
 				></v-autocomplete>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col class="d-inline-flex">
+				<v-btn class="flex-grow-1">Clear</v-btn>
 			</v-col>
 		</v-row>
 	</div>
@@ -36,9 +52,15 @@ export default {
 	data() {
 		return {
 			filter: {
-				name: "test",
-				filterBy: "character",
-				categories: []
+				artist: "test",
+				characters: {
+					list: ["Ahri", "Neeko", "Nidalee"],
+					selected: []
+				},
+				categories: {
+					list: ["Solo", "Yuri"],
+					selected: []
+				}
 			}
 		}
 	}
